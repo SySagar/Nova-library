a1d6803c50fd7b63b382dbd406ab6a729115cb76
SySagar <sysagar07@gmail.com>
2023-05-17 22:50:59 +0530

    feat:added emoji-picker

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -1,71 +1,61 @@
-
-import { EditorContent, useEditor } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import { FaBold, FaHeading, FaItalic, FaListOl, FaListUl, FaStrikethrough, FaUnderline } from 'react-icons/fa'
-import Underline from '@tiptap/extension-underline'
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import {
+  FaBold,
+  FaHeading,
+  FaItalic,
+  FaListOl,
+  FaListUl,
+  FaStrikethrough,
+  FaUnderline,
+  FaSmile,
+} from "react-icons/fa";
+import Underline from "@tiptap/extension-underline";
+import CustomImage from "./Image";
+import EmojiPicker from "emoji-picker-react";
+import { useState } from "react";
+import { EmojiClickData } from "emoji-picker-react";
 
 interface MenuBarProps {
-  editor: any
+  editor: any;
+  setEmojiBoard: React.Dispatch<React.SetStateAction<boolean>>;
+  emojiBoard: boolean;
 }
 
-const MenuBar = ({ editor } : MenuBarProps) => {
+const MenuBar = ({ editor, emojiBoard, setEmojiBoard }: MenuBarProps) => {
   if (!editor) {
-    return null
+    return null;
   }
 
   return (
-    <div className='menu-bar'>
+    <div className="menu-bar" style={{ position: "relative" }}>
       <button
         onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleBold()
-            .run()
-        }
-        className={editor.isActive('bold') ? 'is-active' : ''}
+        disabled={!editor.can().chain().focus().toggleBold().run()}
+        className={editor.isActive("bold") ? "is-active" : ""}
       >
-        <FaBold/>
+        <FaBold />
       </button>
       <button
         onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleItalic()
-            .run()
-        }
-        className={editor.isActive('italic') ? 'is-active' : ''}
+        disabled={!editor.can().chain().focus().toggleItalic().run()}
+        className={editor.isActive("italic") ? "is-active" : ""}
       >
-        <FaItalic/>
+        <FaItalic />
       </button>
       <button
         onClick={() => editor.chain().focus().toogleUnderline().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleItalic()
-            .run()
-        }
-        className={editor.isActive('italic') ? 'is-active' : ''}
+        disabled={!editor.can().chain().focus().toggleItalic().run()}
+        className={editor.isActive("italic") ? "is-active" : ""}
       >
-        <FaUnderline/>
+        <FaUnderline />
       </button>
       <button
         onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleStrike()
-            .run()
-        }
-        className={editor.isActive('strike') ? 'is-active' : ''}
+        disabled={!editor.can().chain().focus().toggleStrike().run()}
+        className={editor.isActive("strike") ? "is-active" : ""}
       >
-        <FaStrikethrough/>
+        <FaStrikethrough />
       </button>
       {/* <button
         onClick={() => editor.chain().focus().toggleCode().run()}
@@ -94,9 +84,9 @@ const MenuBar = ({ editor } : MenuBarProps) => {
       </button> */}
       <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
       >
-        <FaHeading/>
+        <FaHeading />
       </button>
       {/* <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
@@ -130,15 +120,19 @@ const MenuBar = ({ editor } : MenuBarProps) => {
       </button> */}
       <button
         onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
+        className={editor.isActive("bulletList") ? "is-active" : ""}
       >
-        <FaListUl/>
+        <FaListUl />
       </button>
       <button
         onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
+        className={editor.isActive("orderedList") ? "is-active" : ""}
       >
-       <FaListOl/>
+        <FaListOl />
+      </button>
+
+      <button style={{ position: "absolute", right: 0 }} onClick={() => setEmojiBoard(!emojiBoard)}>
+        <FaSmile />
       </button>
       {/* <button
         onClick={() => editor.chain().focus().toggleCodeBlock().run()}
@@ -183,25 +177,41 @@ const MenuBar = ({ editor } : MenuBarProps) => {
         redo
       </button> */}
     </div>
-  )
-}
+  );
+};
 
 export default function Tiptap() {
   const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Underline
-    ],
+    extensions: [StarterKit, Underline, CustomImage],
     content: ``,
-  })
+  });
+
+  const [emojiBoard, setEmojiBoard] = useState(false);
+
+  function onClickEmoji(emojiData: EmojiClickData) {
+    console.log(emojiData.emoji);
+
+    if (editor !== null) {
+      editor.commands.insertContent(emojiData.emoji);
+    }
+  }
 
   return (
-    <div className='text-editor' style={{width:'100%',height:'100%'}}>
-      <div className='menubar'>
-      <MenuBar editor={editor} />
+    <div className="text-editor" style={{ width: "100%", height: "100%" }}>
+      <div className="menubar" style={{position:'relative'}}>
+        <MenuBar
+          editor={editor}
+          emojiBoard={emojiBoard}
+          setEmojiBoard={setEmojiBoard}
+        />
+        {emojiBoard && (
+          <div className="emojis">
+            <EmojiPicker onEmojiClick={onClickEmoji} />
+          </div>
+        )}
       </div>
-      
+
       <EditorContent editor={editor} />
     </div>
-  )
+  );
 }
